6207b92d13c377be64fc9827e5a82500141af832
chinomso <posh.bc@gmail.com>
2021-11-15 09:34:03 +0100

    Add default methods

--- src/main/java/com/looseboxes/mailsender/MailSender.java
+++ src/main/java/com/looseboxes/mailsender/MailSender.java
@@ -1,5 +1,8 @@
 package com.looseboxes.mailsender;
 
+import com.looseboxes.gmailapi.MessageUtil;
+
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -8,6 +11,30 @@ import javax.mail.internet.MimeMessage;
  * @author hp
  */
 public interface MailSender {
- 
+
+    default boolean isSuccessful(Map messageSendingResponse) {
+        return messageSendingResponse != null && !messageSendingResponse.isEmpty();
+    }
+
+    default Map send(String from, String [] to, String subject, String content, String charset, String subtype)
+            throws MessagingException{
+
+        if(from == null || from.isEmpty()) {
+            throw new IllegalArgumentException("Mail sender (from) must be specified");
+        }
+
+        if(to == null || to.length == 0) {
+            throw new IllegalArgumentException("Mail recipient (to) must be specified");
+        }
+
+        if((subject == null || subject.isEmpty()) && (content == null || content.isEmpty())) {
+            throw new IllegalArgumentException("Either message subject or content must be specified");
+        }
+
+        MimeMessage mimeMessage = MessageUtil.createEmail(to, from, subject, content, charset, subtype);
+
+        return send(mimeMessage);
+    }
+
     Map send(MimeMessage mimeMessage) throws MessagingException;
 }
